a651c906183ae5ecc569a62d4eaef68b0e9415db
Dan Morriss <daniel_morriss@yahoo.com.au>
2018-10-15 00:38:30 +1100

    Ok can't make it too big until we support indirect blocks.

--- newmain.c
+++ newmain.c
@@ -7,7 +7,8 @@ int main(int argc, char *argv[])
 
     int i = 0;
     printf("Hello world!\r\n");
-    printf("received %d arguments\r\n", argc);
+    //printf("5 + 5 = %d\r\n", 5+ 5);
+    //printf("received %d arguments\r\n", argc);
     puts("\r\n");
     return 0; 
 
