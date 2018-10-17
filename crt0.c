e5ff44ef5d7cce3ff7ed090d048889efbbc5e2df
Dan Morriss <daniel_morriss@yahoo.com.au>
2018-10-17 22:58:16 +1100

    Updated.

--- crt0.c
+++ crt0.c
@@ -6,7 +6,9 @@ extern int main(int argc, char *argv[]);
 
 _bdos_vtable *btvt = NULL;
 
-int _start(_bdos_vtable *my_btvt, int argc, char *argv[])
+int _start(_bdos_vtable *my_btvt, int argc, char *argv[])  __attribute__((section(".start")));
+
+int _start(_bdos_vtable *my_btvt, int argc, char *argv[])  
 {
 
     printf("*** _start() ***\r\n");
