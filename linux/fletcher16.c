98a55ba5ebbaa0954204272f12d3231d9ad595fb
Dan Morriss <daniel.morriss@visionstream.com.au>
2018-10-30 22:29:24 +1100

    run f16 on arbitrary files

--- linux/fletcher16.c
+++ linux/fletcher16.c
@@ -6,7 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <string.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 
 typedef u_int16_t uint16_t;
 typedef u_int32_t uint32_t;
@@ -15,12 +20,39 @@ typedef u_int8_t uint8_t;
 #include "fletcher16.h"
 
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
-    char *text = "Hello world 1\r\n";
+		char *text = NULL;
+		struct stat sbuf;
+		int c = 0, fd = 0, rd =0;
+		uint32_t total = 0;
+		char *p = NULL, *q = NULL;
 
-    printf("fletcher16 result = 0x%x\r\n", fletcher16(text, strlen(text)));
+		for (int i = 0; i < argc; i++) {
+			printf("argv[%d] = '%s'\n", i, argv[i]);
+		}
+		assert(argc == 2);
+		assert(argv[1]);
+		c = stat(argv[1], &sbuf);
+		assert(stat(argv[1], &sbuf) == 0 );
+		printf("size = %lu\r\n", sbuf.st_size);
+		p = malloc(sbuf.st_size);
+		assert(p);
+		q = p;
+		fd = open(argv[1], O_RDONLY);
+		assert(fd != -1);
 
+		rd = read(fd, q, 4096);
+		while (rd > 0) {
+			total += rd;
+			q+=rd;
+			rd = read(fd, q, 4096);
+			}
+
+		assert(total == sbuf.st_size);
+    printf("fletcher16 result = 0x%x\r\n", fletcher16(p, total));
+
+		close(fd);
     exit(0);
 }
 
