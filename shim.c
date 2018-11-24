0d394acb3cf9b5c480e4b92ac81ff975e811fa3e
Dan Morriss <daniel.morriss@visionstream.com.au>
2018-11-24 17:55:18 +1100

    Moved byte order functions off to madlibc.

--- shim.c
+++ shim.c
@@ -439,39 +439,6 @@ int cpmsim_read(struct _device *d, unsigned char *buf, uint32_t size)
     return 0;
 }
 
-bool is_big_endian(void)
-{
-    union {
-        uint32_t i;
-        char c[4];
-    } bint = {0x01020304};
-
-    return bint.c[0] == 1;
-}
-
-uint16_t swap_uint16( uint16_t val )
-{
-    return (val << 8) | (val >> 8 );
-}
-
-uint32_t swap_uint32( uint32_t val )
-{
-    val = ((val << 8) & 0xFF00FF00 ) | ((val >> 8) & 0xFF00FF );
-    return (val << 16) | (val >> 16);
-}
-
-uint16_t nm_uint16(uint16_t val)
-{
-    return (is_big_endian() ? swap_uint16(val) : val);
-
-}
-
-uint32_t nm_uint32(uint32_t val)
-{
-    return (is_big_endian() ? swap_uint32(val) : val);
-
-}
-
 int ls(char *s)
 {
     //printf("ls(%s)\r\n", s);
